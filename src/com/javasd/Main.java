fbf122a05c342ffbf9f72b608c20bed9f062db42
Almir Campos <ac@javasd.com>
2015-10-21 12:09:39 -0700

    First update.

--- src/com/javasd/Main.java
+++ src/com/javasd/Main.java
@@ -3,6 +3,6 @@ package com.javasd;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println( "This is the Test 03");
+	    System.out.println( "This is another update for the Test 03");
     }
 }
